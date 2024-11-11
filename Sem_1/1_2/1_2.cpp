af39e0f16e0508600e52ffeb58e5bf4af74bd19b
Oleg <olegkyrginyan@gmail.com>
2024-11-11 21:20:18 +0500

    Добавил код к 1_1, сделал 1_2, 1_3

---
+++ Sem_1/1_2/1_2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	float a, b;
+	cin >> a >> b;
+	if ((b != 0) && (a != 0))
+	{
+		cout << a / b << endl;
+	}
+	else
+		if (b == 0)
+		{
+			cout << "You can't divide by 0" << endl;
+		}
+		else
+		{
+			cout << 0 << endl;
+		}
+	return 0;
+}
