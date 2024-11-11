dc54ae9a0dd6a3179c7356b6440456559cb30b44
Oleg <olegkyrginyan@gmail.com>
2024-11-12 04:22:22 +0500

    Обновил readme, залил 2_1, 2_2

---
+++ Sem_1/2_1/2_1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int N, sum;
+    cin >> N;
+    int k = 0;
+    sum = 0;
+    while (k <= N)
+    {
+        sum += k;
+        k = k + 1;
+    }
+    cout << sum << endl;
+    return 0;
+}
+
---
+++ Sem_1/2_2/2_2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int prod, N;
+    int k = 1;
+    cin >> N;
+    prod = 1;
+    while (k <= N)
+    {
+        prod = prod * k;
+        k = k + 1;
+    }
+    cout << prod << endl;
+    return 0;
+}
