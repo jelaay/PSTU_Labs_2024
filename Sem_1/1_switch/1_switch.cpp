480374c0917890bc7bb7da191633d2bf61888926
Oleg <olegkyrginyan@gmail.com>
2024-11-12 16:11:26 +0500

    Добавил 1_switch

---
+++ Sem_1/1_switch/1_switch.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a, b;
+    int op;
+    cout << "first, second, 1 - summ, 2 - vich, 3 - umn, 4 - del"<<endl;
+    cin >> a >> b >> op;
+    switch (op)
+    {
+    case 1: cout << a + b << endl; break;
+    case 2: cout << a - b << endl; break;
+    case 3: cout << a * b << endl; break;
+    case 4: 
+    {
+        if ((b != 0) && (a != 0))
+        {
+            cout << a / b << endl;
+        }
+        else
+            if (b == 0)
+            {
+                cout << "You can't divide by 0" << endl;
+            }
+            else
+            {
+                cout << 0 << endl;
+            }
+        break;
+    }
+    default: cout << "wrong znach" << endl;
+    }
+    return 0;
+}
+
