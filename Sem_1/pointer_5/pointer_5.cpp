c08b05054c766d5167d625a8747141be629ffcf4
Oleg <olegkyrginyan@gmail.com>
2024-11-14 17:10:45 +0500

    p_5

---
+++ Sem_1/pointer_5/pointer_5.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_CTYPE, "rus");
+    unsigned int n, fact = 1;
+    unsigned int *f = &fact;
+    
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        *f *= i;
+    }
+    cout << "Факториал " << n << " равен " << fact << endl;
+    return 0;
+}
+
