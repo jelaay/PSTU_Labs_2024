dcba8d34b937f97327b80931acae0b2f8c937a54
Oleg <olegkyrginyan@gmail.com>
2024-11-15 15:04:21 +0500

    добавил 2_3, обновил readme

---
+++ Sem_1/2_3/2_3.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_CTYPE, "rus");
+    int N,sum,pr;
+    cout << "Введите n: ";
+    cin >> N;
+    sum = 0;
+    for (int i = 1; i <= N; i++)
+    {
+        pr = 1;
+        for (int j=i;j<=i*2;j++)
+        {
+            pr *= j;
+        }
+        sum += pr;
+    }
+    cout << sum << endl;
+    return 0;
+}
