a56e18262b4ad93115d4d8ecdb70c03b76736099
Oleg <olegkyrginyan@gmail.com>
2024-11-21 01:38:58 +0500

    Написал код к оставшимся заданиям 2_?

---
+++ Sem_1/2_4/2_4.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_CTYPE, "rus");
+    int n,k,z;
+    cout << "Введите основание треугольника, оно должно быть нечётным и больше трёх: ";
+    cin >> n;
+    k = 0;
+    z = 1;
+    if ((n % 2 != 0) && (n > 3))
+    {
+        for (int i = 1; i <= ((n / 2) + 1); i++)
+        {
+            for (int j = 1; j <= ((n / 2)-k); j++)
+            {
+                cout << " ";
+            }
+            k += 1;
+            for (int g = 1; g <= z; g++)
+            {
+                cout << "*";
+            }
+            z = z + 2;
+            cout << "" << endl;
+        }
+    }
+    else
+    {
+        cout << "Введите корректное значение!" << endl;
+    }
+    return 0;
+}
---
+++ Sem_1/2_6/2_6.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_CTYPE, "rus");
+    int n, k, z;
+    cout << "Введите основание треугольника, оно должно быть больше двух: ";
+    cin >> n;
+    k = n;
+    z = n ;
+    if (n > 2)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= (n - k); j++)
+            {
+                cout << " ";
+            }
+            k -= 1;
+            for (int g = 1; g <= z; g++)
+            {
+                cout << "*";
+            }
+            z = z - 1;
+            cout << "" << endl;
+        }
+    }
+    else { cout << "Введите корректное значение!" << endl; }
+    return 0;
+}
