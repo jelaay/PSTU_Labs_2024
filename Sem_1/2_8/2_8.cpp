50930d7123d8b4840bb5d1daaa2b8fdd4c3b6b67
Oleg <olegkyrginyan@gmail.com>
2024-11-21 17:32:56 +0500

    Сделал 2_8, доделал блок-схему 2_7

---
+++ Sem_1/2_8/2_8.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    setlocale(LC_CTYPE, "rus");
+    int n,stars=1;
+    cout << "Введите основание треугольника, оно должно быть больше двух: ";
+    cin >> n;
+    if (n > 2)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= stars; j++)
+            {
+                cout << " *";
+            }
+            stars += 1;
+            cout << endl;
+        }
+        
+        
+    }
+    else { cout << "Введите корректное значение!" << endl; }
+    return 0;
+}
